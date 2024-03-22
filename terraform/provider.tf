03f5c2dbf4570e39444a0ca6c365795a039630d6
Kamil Buruk <kamil.buruk@bipro-service.gmbh>
2024-03-22 16:26:41 +0100

    lock hinzugefügt und Einträge zum Provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -20,3 +20,28 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
+
+#Storage account benötigt Provider > Microsoft.Storage
+resource "azurerm_resource_provider_registration" "providerMicrosoftStorage" {
+  name = "Microsoft.Storage"
+}
+
+#Log Analytics Dashboard benötigt Provider > Microsoft.OperationalInsights
+resource "azurerm_resource_provider_registration" "providerMicrosoftOperationalInsights" {
+  name = "Microsoft.OperationalInsights"
+}
+
+# App Environment benötigt Provider > Microsoft.App
+resource "azurerm_resource_provider_registration" "providerMicrosoftApp" {
+  name = "Microsoft.App"
+}
+
+#Container Registry benötigt Provider > Microsoft.ContainerRegistry
+resource "azurerm_resource_provider_registration" "providerMicrosoftContainerRegistry" {
+  name = "Microsoft.ContainerRegistry"
+}
+
+#Key Vault benötigt Provider > Microsoft.KeyVault (wird evtl. nicht benötigt)
+resource "azurerm_resource_provider_registration" "providerMicrosoftKeyVault" {
+  name = "Microsoft.KeyVault"
+}
