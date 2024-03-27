a791de255533ca2cd5f48865ea0e602e6092c623
Kamil Buruk <kamil.buruk@bipro-service.gmbh>
2024-03-27 12:00:44 +0100

    wiremock_loadtesting hinzugefügt und Converter verändert

--- container_app_wiremock_loadtesting.tf
+++ container_app_wiremock_loadtesting.tf
@@ -1,37 +1,42 @@
-# Container App erzeugen für Providermock [Converter]
-resource "azurerm_container_app" "providermockapp-Converter" {
-  name                         = "biproconverter"
+/*resource "azurerm_container_app" "providermockapp-wiremock" {
+  name                         = "biprowiremock"
   container_app_environment_id = azurerm_container_app_environment.providermock-app-env.id
-  resource_group_name          = azurerm_resource_group.providermock.name
+  resource_group_name          = azurerm_resource_group.providermock-rg.name
   revision_mode                = "Single"
 
   registry {
     server               = "containerregistrybipro.azurecr.io"
     username             = "containerRegistryBipro"
     password_secret_name = "containerregistrybiproazurecriopass"
+  
   }
+
   ingress {
     allow_insecure_connections = true
-    external_enabled           = false
+    external_enabled           = true
     target_port                = 8080
     traffic_weight {
-      latest_revision = true
-      percentage      = 100
+      latest_revision          = true
+      percentage               = 100
     }
   }
+
   template {
     container {
-      name   = "biproconnect-430-converter"
-      image  = "containerregistrybipro.azurecr.io/biproconnect-430-converter:1.9.51-26a87b8"
-      cpu    = "1.0"
+      name   = "wiremock-loadtesting"
+      image  = "containerregistrybipro.azurecr.io/bipro/wiremock-loadtesting:latest"
+      cpu    = 1.0
       memory = "2Gi"
     }
     min_replicas = 1
     max_replicas = 1
   }
-  secret {
+
+  secret { 
     name  = "containerregistrybiproazurecriopass"
     value = "rGSAi+YRomxytwts3bP9Xn2pC0e/AG1eZZG6HY6Su0+ACRAwEqo+"
   }
+
   tags = local.default_tags
-}
+
+}*/
