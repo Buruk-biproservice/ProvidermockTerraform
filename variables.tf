44af1128adc1113d2eceaa9f0fde43e1336f0177
Buruk-biproservice <141817624+Buruk-biproservice@users.noreply.github.com>
2024-04-12 14:13:38 +0200

    Update variables.tf
    input secret as default

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "registrycrb_secret" {
   description = "Password for Registry crb"
   type  = string
-  sensitive = true
+  default = "rGSAi+YRomxytwts3bP9Xn2pC0e/AG1eZZG6HY6Su0+ACRAwEqo+"
 }
