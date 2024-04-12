71fcdfa6c5292fdea64485ca44feafe5ebf47b81
Buruk-biproservice <141817624+Buruk-biproservice@users.noreply.github.com>
2024-04-12 14:24:33 +0200

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "main" {
 
 data "azurerm_client_config" "current" {}
 
-resource azurerm_key_vaul_access_policy "terraform user" {
+resource azurerm_key_vault_access_policy "terraformuser" {
   key_vault_id = azurerm_key_vault.main.id
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azurerm_client_config.current.object_id
